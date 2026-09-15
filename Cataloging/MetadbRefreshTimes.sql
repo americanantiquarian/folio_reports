b4d230afada7fd7f9c8e3d131401cb19cafdd580
KHaley <kathleen.haley.aas@gmail.com>
2026-09-15 16:10:17 -0400

    Update MetadbRefreshTimes.sql

--- Cataloging/MetadbRefreshTimes.sql
+++ Cataloging/MetadbRefreshTimes.sql
@@ -12,7 +12,7 @@ returns table(
 as $$
 select tu.schema_name as "schema",
 	tu.table_name as "table",
-	to_char(tu.last_update - (tu.elapsed_real_time::text || ' minutes')::interval), 'YYYY-MM-DD') as start_day,
+	to_char(tu.last_update::text::timestamp - (tu.elapsed_real_time::text || ' minutes')::interval), 'YYYY-MM-DD') as start_day,
 	to_char(tu.last_update - (tu.elapsed_real_time::text || ' minutes')::interval, 'HH12:MI:SS AM') as start_time,
 	to_char(tu.last_update, 'HH12:MI:SS AM') as end_time
 from metadb.table_update tu
